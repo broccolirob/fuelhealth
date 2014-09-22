f70e2e4f501e793b1abd2ea06981c3c16b6b37dd
Robert Schneider <rschn@uga.edu>
2014-09-22 11:44:51 -0700

    uncommented database

--- fuelhealth/settings.py
+++ fuelhealth/settings.py
@@ -111,7 +111,7 @@ EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
 
 import dj_database_url
 
-# DATABASES['default'] = dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
