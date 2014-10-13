4c3984bcb8956f95e98062bc11b0d5e2e4c6e9f8
Rudy Mutter <rudy@yetihq.com>
2014-10-13 16:07:54 -0700

    Update utils.py

--- apps/news/utils.py
+++ apps/news/utils.py
@@ -2,7 +2,7 @@ import datetime
 from django.utils.timezone import utc
 from apps.news.models import Article
 
-
+# This file could use some docuemntation love for your score algorithms
 def calculate_score(votes, item_hour_age, gravity=1.8):
     return (votes - 1) / pow((item_hour_age + 2), gravity)
 
@@ -15,6 +15,8 @@ def score(article, gravity=1.8, timebase=120):
 
 
 def top_articles(top=180, consider=1000):
-    latest_articles = Article.objects.all().order_by('-created_at')[:consider]
+    latest_articles = Article.objects.order_by('-created_at')[:consider]
+    # Instead of sorting on a list with the score, then making another list which dumps the score
+    # you could just use http://stackoverflow.com/questions/3216398/sorting-by-arbitrary-lambda-in-python
     ranked_articles = sorted([(score(article), article) for article in latest_articles], reverse=True)
     return [article for _, article in ranked_articles][:top]
